6c35fbb952ad7abba4762b61a68514e664f2ab0f
Muhammad Thariq <115327938+mightythorrr@users.noreply.github.com>
2022-10-09 11:19:17 +0700

    Create index.php
    How to make an triangle number using looping for

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+for ($x=5; $x >= 1; $x--){
+	for ($y=1; $y <=$x; $y++){
+		echo "$y";
+	}
+	echo "<br>";}
+         
+    for ($x=1; $x <= 5; $x++){
+        for ($y=1; $y <=$x; $y++){
+            echo "$y";
+        }
+        echo "<br>";} 
