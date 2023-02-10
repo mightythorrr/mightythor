dc6d4ed5a65fbdc723a32865dad4aa5f8828aae3
Muhammad Thariq <115327938+mightythorrr@users.noreply.github.com>
2023-02-10 22:47:01 +0700

    Create triangle.php

---
+++ triangle.php
@@ -0,0 +1,31 @@
+<?php 
+//soal nomor 4//
+for ($x=5; $x >= 1; $x--){
+	for ($y=1; $y <=$x; $y++){
+		echo "$y";
+	}
+	echo "<br>";}
+         
+    for ($x=1; $x <= 5; $x++){
+        for ($y=1; $y <=$x; $y++){
+            echo "$y";
+        }
+        echo "<br>";} 
+    
+        echo "<br>";
+
+        echo "<br>";
+       
+    //soal nomor 5//
+    for ($a=1; $a <=5; $a++){
+        for ($b=$a; $b>=1; $b--){
+            echo "$a";
+        }
+            echo "<br>";}  
+    
+    for ($a=4; $a>=1; $a--){
+        for ($b=1; $b<=$a; $b++){
+            echo "$a";
+        }
+            echo "<br>";}  
+?>
